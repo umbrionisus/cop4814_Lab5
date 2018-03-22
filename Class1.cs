f8965d44a59be066d6998cc63361aada8069b18b
umbrionisus <37598763+umbrionisus@users.noreply.github.com>
2018-03-22 17:24:05 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlightLibrary
+{
+    public class Flight
+    {
+        public int FlightNumber { get; set; }
+        public string DateandTime { get; set; }
+        public string OrigAirport { get; set; }
+        public string DestAirport { get; set; }
+        public double Fare { get; set; }
+        public string Seats { get; set; }
+
+        public Flight ( int FlightNumber, string DateandTime, string OrigAirport, string DestAirport, double Fare, string Seats )
+        {
+            this.FlightNumber = FlightNumber;
+            this.DateandTime = DateandTime;
+            this.OrigAirport = OrigAirport;
+            this.DestAirport = DestAirport;
+            this.Fare = Fare;
+            this.Seats = Seats;
+        }
+    }
+}
